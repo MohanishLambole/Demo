b12aaa0d7cfbabe2a6229253dcc26c4713152cb8
Mayuresh Dagale <mayuresh.j.dagale@gmail.com>
2026-03-04 13:01:13 +0530

    js updated

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const add = document.querySelector("#add");
+const form = document.querySelector("#form");
+const result = document.querySelector("#result");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const data = new FormData(form);
+    let addition = 0;
+
+    for(const n of data.values()){
+        addition += Number(n);
+    }
+
+    result.textContent = `${addition}`;
+})
